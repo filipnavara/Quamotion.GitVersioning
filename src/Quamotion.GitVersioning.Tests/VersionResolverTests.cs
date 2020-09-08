199eb2125811943b9fec3e16a6a1fdf4b3c5ecfe
Frederik Carlier <frederik.carlier@quamotion.mobi>
2020-09-08 21:27:12 +0200

    Benchmarks: Add NB.GV-based tests

--- src/Quamotion.GitVersioning.Tests/VersionResolverTests.cs
+++ src/Quamotion.GitVersioning.Tests/VersionResolverTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging.Abstractions;
+using Nerdbank.GitVersioning;
 using Quamotion.GitVersioning.Git;
 using System;
 using System.IO;
@@ -25,6 +26,24 @@ namespace Quamotion.GitVersioning.Tests
             VersionResolver resolver = new VersionResolver(repository, versionPath, NullLogger<VersionResolver>.Instance);
 
             var version = resolver.GetVersion();
+            Assert.Equal(expectedVersion, version);
+        }
+
+        [Theory]
+        [InlineData("xunit", "version.json", "0.1.0-pre.98")] // https://github.com/xunit/xunit
+        [InlineData("SuperSocket", "version.json", "2.0.0-beta7")] // https://github.com/kerryjiang/SuperSocket
+        [InlineData("Cuemon", "version.json", "6.0.0-preview.10")] // https://github.com/gimlichael/Cuemon
+        public void GetNbgvVersionTest(string repositoryName, string versionPath, string expectedVersion)
+        {
+            string path =
+                Path.Combine(
+                    Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+                    @"Source\Repos",
+                    repositoryName);
+
+            var oracleA = VersionOracle.Create(path);
+            var version = oracleA.CloudBuildNumber;
+
             Assert.Equal(expectedVersion, version);
         }
     }
