bf7803327bba078e3f0a69c439ab5da25172e35b
Frederik Carlier <frederik.carlier@quamotion.mobi>
2020-09-11 11:51:20 +0200

    Update unit tests

--- src/Quamotion.GitVersioning.Tests/VersionResolverTests.cs
+++ src/Quamotion.GitVersioning.Tests/VersionResolverTests.cs
@@ -1,19 +1,27 @@
-﻿using Microsoft.Extensions.Logging.Abstractions;
-using Nerdbank.GitVersioning;
+﻿using Nerdbank.GitVersioning;
 using Quamotion.GitVersioning.Git;
 using System;
 using System.IO;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Quamotion.GitVersioning.Tests
 {
     public class VersionResolverTests
     {
+        private readonly ITestOutputHelper output;
+
+        public VersionResolverTests(ITestOutputHelper output)
+        {
+            this.output = output ?? throw new ArgumentNullException(nameof(output));
+        }
+
         [Theory]
-        [InlineData("xunit", "version.json", "0.1.0-pre.{height}.99")] // https://github.com/xunit/xunit
-        [InlineData("xunit2", "version.json", "0.1.0-pre.{height}.99")] // https://github.com/xunit/xunit, shared clone
-        [InlineData("SuperSocket", "version.json", "2.0.0-beta7.13")] // https://github.com/kerryjiang/SuperSocket
-        [InlineData("Cuemon", "version.json", "6.0.0-preview.{height}.11")] // https://github.com/gimlichael/Cuemon
+        [InlineData("xunit", "version.json", "0.1.0-pre.98")] // https://github.com/xunit/xunit
+        [InlineData("xunit2", "version.json", "0.1.0-pre.98")] // https://github.com/xunit/xunit, shared clone
+        [InlineData("SuperSocket", "version.json", "2.0.0-beta7")] // https://github.com/kerryjiang/SuperSocket
+        [InlineData("Cuemon", "version.json", "6.0.0-preview.10")] // https://github.com/gimlichael/Cuemon
+        [InlineData("NerdBank.GitVersioning", "version.json", "3.3.22-alpha")] // https://github.com/dotnet/nerdbank.GitVersioning/
         public void GetVersionTest(string repositoryName, string versionPath, string expectedVersion)
         {
             string path =
@@ -23,7 +31,7 @@ namespace Quamotion.GitVersioning.Tests
                     repositoryName);
 
             GitRepository repository = new GitRepository(path);
-            VersionResolver resolver = new VersionResolver(repository, versionPath, NullLogger<VersionResolver>.Instance);
+            VersionResolver resolver = new VersionResolver(repository, versionPath, output.BuildLoggerFor<VersionResolver>());
 
             var version = resolver.GetVersion();
             Assert.Equal(expectedVersion, version);
@@ -33,6 +41,7 @@ namespace Quamotion.GitVersioning.Tests
         [InlineData("xunit", "version.json", "0.1.0-pre.98")] // https://github.com/xunit/xunit
         [InlineData("SuperSocket", "version.json", "2.0.0-beta7")] // https://github.com/kerryjiang/SuperSocket
         [InlineData("Cuemon", "version.json", "6.0.0-preview.10")] // https://github.com/gimlichael/Cuemon
+        [InlineData("NerdBank.GitVersioning", "version.json", "3.3.22-alpha")] // https://github.com/dotnet/nerdbank.GitVersioning/
         public void GetNbgvVersionTest(string repositoryName, string versionPath, string expectedVersion)
         {
             string path =
